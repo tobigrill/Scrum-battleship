371350018d894fe2332e1b0014ee5e05bd707930
dwuerfl <97949138+dwuerfl@users.noreply.github.com>
2023-03-19 10:56:02 +0100

    Add files via upload
    All ship types can be placed twice now, atm no rotate-function

--- Battleship-ship-placement.py
+++ Battleship-ship-placement.py
@@ -1,7 +1,6 @@
 '''
 David Wuerfl
-18.03.23
-Battleship ship placement
+19.03.2023
 '''
 
 import sys
@@ -11,12 +10,13 @@ from PyQt6.QtGui import *
 from PyQt6.QtWidgets import *
 
 class MainWindow(QMainWindow):
-    app = QApplication(sys.argv)
+    
     def __init__(self):
         super().__init__()
-        
+        self.selected_ship = None
+        self.ship_size = None
         self.setWindowTitle("Battleship-test")
-        self.setFixedSize(QSize(700, 680)) 
+        self.setFixedSize(QSize(700, 680))
 
         self.menu = self.menuBar()
         self.options_menu = self.menu.addMenu("Options")
@@ -24,23 +24,30 @@ class MainWindow(QMainWindow):
         self.quit_action = QAction("Quit", self)
         self.quit_action.triggered.connect(QApplication.instance().quit)
         self.options_menu.addAction(self.quit_action)
-        
+        self.ship_counts = {
+            "2": 0,
+            "3": 0,
+            "4": 0,
+            "5": 0
+        }
         self.size = 10
         self.sizeb = 10
         self.button_size = 50
         self.button_sizeb = 70
-    
+
         self.options_layout = QVBoxLayout()
-        
+
         self.radioButton1 = QRadioButton("Patrol Boat")
-        self.radioButton1.toggled.connect(lambda:self.set_ship_size(2))
+
+        self.radioButton1.toggled.connect(lambda checked, size=2: self.set_ship_size(size))
+
         self.radioButton2 = QRadioButton("Submarine")
-        self.radioButton2.toggled.connect(lambda:self.set_ship_size(3))
+        self.radioButton2.toggled.connect(lambda checked, size=3: self.set_ship_size(size))
         self.radioButton3 = QRadioButton("Destroyer")
-        self.radioButton3.toggled.connect(lambda:self.set_ship_size(1))
+        self.radioButton3.toggled.connect(lambda checked, size=4: self.set_ship_size(size))
         self.radioButton4 = QRadioButton("Carrier")
-        self.radioButton4.toggled.connect(lambda:self.set_ship_size(4))
-        
+        self.radioButton4.toggled.connect(lambda checked, size=5: self.set_ship_size(size))
+
         self.options_layout.addWidget(self.radioButton1)
         self.options_layout.addWidget(self.radioButton2)
         self.options_layout.addWidget(self.radioButton3)
@@ -48,15 +55,15 @@ class MainWindow(QMainWindow):
 
         self.options_frame = QFrame()
         self.options_frame.setLayout(self.options_layout)
-        self.options_dock = QDockWidget("Options", self)
+        self.options_dock = QDockWidget("Ships", self)
         self.options_dock.setWidget(self.options_frame)
         self.addDockWidget(Qt.DockWidgetArea.RightDockWidgetArea, self.options_dock)
 
         self.ships = []
-    
+
         self.grid_layout = QGridLayout()
         self.grid_layout.setSpacing(0)
-        
+
         self.button_fields = []
         for i in range(self.size):
             row = []
@@ -68,14 +75,83 @@ class MainWindow(QMainWindow):
                 self.grid_layout.addWidget(button, i, j)
                 row.append(button)
             self.button_fields.append(row)
-    
+        for i in range(self.size):
+            for j in range(self.size):
+                button = self.button_fields[i][j]
+                button.clicked.connect(lambda _, x=i, y=j: self.place_ship(x, y))
+
         self.field_widget = QWidget()
         self.field_widget.setLayout(self.grid_layout)
         self.scroll_area = QScrollArea()
         self.scroll_area.setWidgetResizable(True)
         self.scroll_area.setWidget(self.field_widget)
         self.setCentralWidget(self.scroll_area)
+    def set_ship_size(self, size=2):
+        self.ship_size = size
+        if size == 2:
+            self.selected_ship = "2"
+        elif size == 3:
+            self.selected_ship = "3"
+        elif size == 4:
+            self.selected_ship = "4"
+        elif size == 5:
+            self.selected_ship = "5"
+        if self.ship_counts[self.selected_ship] >= 2:
+            if size == 2:
+                self.radioButton1.setEnabled(False)
+            elif size == 3:
+                self.radioButton2.setEnabled(False)
+            elif size == 4:
+                self.radioButton3.setEnabled(False)
+            elif size == 5:
+                self.radioButton4.setEnabled(False)
+    
+    def check_valid_placement(self, x, y, orientation):
+        endpoints = self.find_possible_endpoints(x, y, orientation)
+        for endpoint in endpoints:
+            if self.button_fields[endpoint[0]][endpoint[1]].property("occupied"):
+                return False
+        return True
 
+    def occupy_cells(self, x, y, orientation):
+        endpoints = self.find_possible_endpoints(x, y, orientation)
+        for endpoint in endpoints:
+            button = self.button_fields[endpoint[0]][endpoint[1]]
+            button.setProperty("occupied", True)
+            button.setStyleSheet("background-color: #66b3ff")
+            button.setText(self.selected_ship)
+            button.setEnabled(False)
+    
+    def place_ship(self, x, y):
+        if self.selected_ship is None:
+            QMessageBox.warning(self, "No ship selected", "Please select a ship first.")
+            return
+        if self.ship_counts[self.selected_ship] >= 2:
+            QMessageBox.warning(self, "Ship limit reached", f"You cannot place any more {self.selected_ship} ships.")
+            return
+        if self.check_valid_placement(x, y, "right"):
+            self.occupy_cells(x, y, "right")
+            ship_num = self.ship_counts[self.selected_ship] + 1
+            self.ships.append((self.selected_ship, self.find_possible_endpoints(x, y, "right")))
+            for i, j in self.find_possible_endpoints(x, y, "right"):
+                self.button_fields[i][j].setProperty("occupied", True)
+            self.ship_counts[self.selected_ship] += 1
+            if all(count == 2 for count in self.ship_counts.values()):
+                QMessageBox.information(self, "All ships placed", "All ships have been placed.")
+
+    def find_possible_endpoints(self, x, y, orientation):
+        endpoints = []
+        if orientation == "right":
+            if y + self.ship_size <= self.size:
+                for j in range(y, y + self.ship_size):
+                    endpoints.append((x, j))
+        elif orientation == "down":
+            if x + self.ship_size <= self.size:
+                for i in range(x, x + self.ship_size):
+                    endpoints.append((i, y))
+        return endpoints
+
+            
 app = QApplication(sys.argv)
 window = MainWindow()
 window.show()
