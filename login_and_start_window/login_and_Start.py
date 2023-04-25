641310b4068186da9adc3da26a821195df100084
nickmair <105418545+nickmair@users.noreply.github.com>
2023-04-25 09:12:00 +0200

    Visual Update

--- login_and_start_window/login_and_Start.py
+++ login_and_start_window/login_and_Start.py
@@ -1,8 +1,9 @@
 import hashlib
 import sys
 
-from PyQt6.QtWidgets import  QGridLayout, QLineEdit, QMainWindow, QWidget, QVBoxLayout, QLabel, QPushButton, QApplication
+from PyQt6.QtWidgets import QDialog, QGridLayout, QLineEdit, QMainWindow, QWidget, QVBoxLayout, QLabel, QPushButton, QApplication
 from PyQt6.QtCore import Qt
+from PyQt6.uic import loadUi
 import sys
 
 
@@ -52,7 +53,6 @@ class WelcomeWindow(QMainWindow):
                 transition: all 0.3s ease-in-out;
                 box-shadow: 2px 2px 2px #999999;
             }
-
             #START:hover {
                 background-color: #999999;
                 border-color: #666666;
@@ -60,9 +60,7 @@ class WelcomeWindow(QMainWindow):
                 box-shadow: none;
                 transform: translate(2px, 2px);
             }
-
           
-
             """)
 
         # Fenstergröße einstellen
@@ -87,48 +85,24 @@ class WelcomeWindow(QMainWindow):
 
 
 
-class LoginWindow(QMainWindow):
+class LoginWindow(QDialog):
     users = {}
     def __init__(self):
+        super(LoginWindow, self).__init__()
+        loadUi("login.ui",self)
         
-        super().__init__()
-
-        self.setWindowTitle("Login")
-
-        # Zentrales Widget erstellen
-        central_widget = QWidget()
-        self.setCentralWidget(central_widget)
-
-        # Layout erstellen
-        layout = QVBoxLayout()
-
-        # Labels und Eingabefelder erstellen
-        self.username_label = QLabel("Benutzername:")
-        self.username_input = QLineEdit()
-        self.password_label = QLabel("Passwort:")
-        self.password_input = QLineEdit()
-        self.password_input.setEchoMode(QLineEdit.EchoMode.Password)
 
         # Buttons erstellen
-        self.login_button = QPushButton("Einloggen")
+
         self.login_button.clicked.connect(self.login)
-        self.cancel_button = QPushButton("Abbrechen")
+        
         self.cancel_button.clicked.connect(self.close)
-        self.create_button = QPushButton("Neuen Benutzer erstellen")
+        
         self.create_button.clicked.connect(self.create_user_window)
 
-        # Layout anpassen
-        layout.addWidget(self.username_label)
-        layout.addWidget(self.username_input)
-        layout.addWidget(self.password_label)
-        layout.addWidget(self.password_input)
-        layout.addWidget(self.login_button)
-        layout.addWidget(self.create_button)
-        layout.addWidget(self.cancel_button)
+  
 
-        central_widget.setLayout(layout)
-
-        # Benutzerdaten
+        
         
 
     def authenticate(self, username, password):
@@ -148,47 +122,21 @@ class LoginWindow(QMainWindow):
             self.close()
         else:
             print("Falscher Benutzername oder Passwort.")
-
     
     def create_user_window(self):
-        self.setWindowTitle("Neuen Benutzer erstellen")
+       
+        self.hide()
 
-        # Zentrales Widget erstellen
-        central_widget = QWidget()
-        self.setCentralWidget(central_widget)
+    # Load create user window UI
+        
+        create_user = loadUi("create_user.ui", self)
 
-        # Layout erstellen
-        layout = QVBoxLayout()
-
-        # Labels und Eingabefelder erstellen
-        self.username_label = QLabel("Benutzername:")
-        self.username_input = QLineEdit()
-        self.password_label = QLabel("Passwort:")
-        self.password_input = QLineEdit()
-        self.password_input.setEchoMode(QLineEdit.EchoMode.Password)
-        self.password_repeat_label = QLabel("Passwort wiederholen:")
-        self.password_repeat_input = QLineEdit()
-        self.password_repeat_input.setEchoMode(QLineEdit.EchoMode.Password)
-
-        # Buttons erstellen
-        self.create_button = QPushButton("Benutzer erstellen")
+    # Connect buttons
         self.create_button.clicked.connect(self.create_user)
-        self.cancel_button = QPushButton("Abbrechen")
         self.cancel_button.clicked.connect(self.close)
+        create_user.show()
 
-        # Layout anpassen
-        layout.addWidget(self.username_label)
-        layout.addWidget(self.username_input)
-        layout.addWidget(self.password_label)
-        layout.addWidget(self.password_input)
-        layout.addWidget(self.password_repeat_label)
-        layout.addWidget(self.password_repeat_input)
-        layout.addWidget(self.create_button)
-        layout.addWidget(self.cancel_button)
-
-        central_widget.setLayout(layout)
-
-        # Benutzerdaten
+  
         
 
     def create_user(self):
