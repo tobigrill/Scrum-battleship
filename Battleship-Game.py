7720ce2e54f45565a90108a2d61e3e19bdf5716d
dwuerfl <97949138+dwuerfl@users.noreply.github.com>
2023-04-25 12:10:28 +0200

    added unique ship ids, graphic changes

--- Battleship-Game.py
+++ Battleship-Game.py
@@ -1,6 +1,6 @@
 '''
 David Wuerfl
-14.04.2023
+25.04.2023
 Battleship-Ship-Placement
 '''
 
@@ -17,7 +17,7 @@ class In_Game_UI(QWidget):
     def __init__(self):
         super().__init__()
         grid = QGridLayout()
-        button_size = QSize(80, 80)
+        button_size = QSize(70, 70)
         button_margin = 0
         
         
@@ -35,7 +35,8 @@ class In_Game_UI(QWidget):
                 button = QPushButton()
                 button.setFixedSize(button_size)
                 button.setContentsMargins(button_margin, button_margin, button_margin, button_margin)
-                button.setStyleSheet("background-color: #e6f3ff")
+                button.setStyleSheet("QPushButton {background-color:#ccf2ff ; color: white; border: 0.5px solid black;}"
+                                   "QPushButton:hover {background-color:#cce6ff  ;}")
                 grid.addWidget(button, i, j+1)
 
         for i in range(1, 11):
@@ -49,7 +50,9 @@ class In_Game_UI(QWidget):
                 button = QPushButton()
                 button.setFixedSize(button_size)
                 button.setContentsMargins(button_margin, button_margin, button_margin, button_margin)
-                button.setStyleSheet("background-color: #e6f3ff")
+                button.setEnabled(False)
+                button.setStyleSheet("QPushButton {background-color:#ccf2ff ; color: white; border: 0.5px solid black;}"
+                                   "QPushButton:hover {background-color:#cce6ff  ;}")
                 grid.addWidget(button, i, j+14)
 
         for i in range(1, 11):
@@ -57,9 +60,15 @@ class In_Game_UI(QWidget):
             label.setAlignment(Qt.AlignmentFlag.AlignCenter)
             grid.addWidget(label, i, 25, 1, 1)
         
-        shoot_button = QPushButton("Shoot")
-        shoot_button.setFixedSize(160, 50)
-        grid.addWidget(shoot_button, 13, 1, 1, 6)
+        
+        buttonQ = QPushButton("X")
+        buttonQ.setStyleSheet("QPushButton {background-color: red; color: white; border: 2px solid black;}"
+                                   "QPushButton:hover {background-color: #cc0000;}")
+        buttonQ.setFixedSize(QSize(20, 20))
+        buttonQ.clicked.connect(self.close)
+        grid.addWidget(buttonQ, 0,25)
+        
+        
         
         ships_label = QLabel("Ships Left")
         ships_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
@@ -77,29 +86,20 @@ class In_Game_UI(QWidget):
         self.showFullScreen()
         self.show()
 
-
         
-class MainWindow(QMainWindow):
+class MainWindow(QWidget):
+    
+
     
     def __init__(self):
         super().__init__()
-        
+        self.ship_num = 2
         self.selected_ship = None
         self.ship_size = None
         self.setWindowTitle("Place your Ships")
         self.showFullScreen()
-        #self.setFixedSize(QSize(700, 680))
 
-        self.menu = self.menuBar()
-        self.options_menu = self.menu.addMenu("Options")
-
-        self.quit_action = QAction("Quit", self)
-        self.quit_action.triggered.connect(QApplication.instance().quit)
-        self.options_menu.addAction(self.quit_action)
-        
-        self.reset_action = QAction("Reset", self)
-        self.reset_action.triggered.connect(self.reset_game)
-        self.options_menu.addAction(self.reset_action)
+     
         
         self.ship_counts = {
             "2": 0,
@@ -108,11 +108,11 @@ class MainWindow(QMainWindow):
             "5": 0
         }
         
-        self.size = 10
-        self.button_size = QSize(80, 80)
+        
+        self.button_size = QSize(70, 70)
         button_margin = 0
 
-        self.options_layout = QVBoxLayout()
+        self.grid_layout = QGridLayout()
         self.radioButton1 = QRadioButton("Patrol Boat")
         self.radioButton1.toggled.connect(lambda checked, size=2: self.set_ship_size(size))
         self.radioButton2 = QRadioButton("Submarine")
@@ -124,23 +124,29 @@ class MainWindow(QMainWindow):
         self.vertical_checkbox = QCheckBox("Vertical")
         self.buttonC = QPushButton("Confirm")
         self.buttonC.setEnabled(False)
+        self.buttonC.setFixedSize(QSize(150, 20))
         self.buttonC.clicked.connect(self.confirm_placement)
+        self.buttonQ = QPushButton("X")
+        self.buttonQ.setStyleSheet("QPushButton {background-color: red; color: white; border: 2px solid black;}"
+                                   "QPushButton:hover {background-color: #cc0000;}")
+        self.buttonQ.setFixedSize(QSize(20, 20))
+        self.buttonQ.clicked.connect(self.close)
+        self.buttonR = QPushButton("Reset")
+        self.buttonR.setFixedSize(QSize(150, 20))
+        self.buttonR.setStyleSheet("QPushButton {background-color: #c0c0c0; color: black; border: 2px solid black;}"
+                                   "QPushButton:hover {background-color:#999999 ;}")
         
-        self.options_layout.addWidget(self.vertical_checkbox)
-        self.options_layout.addWidget(self.radioButton1)
-        self.options_layout.addWidget(self.radioButton2)
-        self.options_layout.addWidget(self.radioButton3)
-        self.options_layout.addWidget(self.radioButton4)
-        self.options_layout.addWidget(self.buttonC)
-        self.options_frame = QFrame()
-        self.options_frame.setLayout(self.options_layout)
-        self.options_dock = QDockWidget("Ships", self)
-        self.options_dock.setWidget(self.options_frame)
-        self.options_dock.setMinimumSize(700,1000)
-        self.options_dock.setMaximumSize(700,1000)
-        self.addDockWidget(Qt.DockWidgetArea.RightDockWidgetArea, self.options_dock)
+        self.buttonR.clicked.connect(self.reset_game)
+        self.grid_layout.addWidget(self.buttonQ, 0,15)
+        self.grid_layout.addWidget(self.vertical_checkbox, 1,13)
+        self.grid_layout.addWidget(self.radioButton1, 2,13)
+        self.grid_layout.addWidget(self.radioButton2, 3,13)
+        self.grid_layout.addWidget(self.radioButton3, 4,13)
+        self.grid_layout.addWidget(self.radioButton4, 5,13)
+        self.grid_layout.addWidget(self.buttonC, 6,13)
+        self.grid_layout.addWidget(self.buttonR,0,13)
         self.ships = []
-        self.grid_layout = QGridLayout()
+        
         self.grid_layout.setVerticalSpacing(0)
         self.grid_layout.setHorizontalSpacing(0)
         self.grid_layout.setSpacing(0)
@@ -148,20 +154,22 @@ class MainWindow(QMainWindow):
         
         
         
-        for i in range(self.size):
+        for i in range(10):
             row = []
         
-            for j in range(self.size):
+            for j in range(10):
                 button = QPushButton()
                 button.setObjectName(f"{i},{j}")
                 button.setFixedSize(self.button_size)
-                button.setContentsMargins(button_margin, button_margin, button_margin, button_margin)
-                button.setStyleSheet("background-color: #e6f3ff")
+                button.setContentsMargins(0, 0, 0, 0)
+                
+                button.setStyleSheet("QPushButton {background-color:#ccf2ff ; color: white; border: 0.5px solid black;}"
+                                   "QPushButton:hover {background-color:#cce6ff  ;}")
                 self.grid_layout.addWidget(button, i+1, j+1)
                 row.append(button)
             self.button_fields.append(row)
-        for i in range(self.size):
-            for j in range(self.size):
+        for i in range(10):
+            for j in range(10):
                 button = self.button_fields[i][j]
                 button.clicked.connect(lambda _, x=i, y=j: self.place_ship(x, y))
                 
@@ -174,13 +182,9 @@ class MainWindow(QMainWindow):
             label.setAlignment(Qt.AlignmentFlag.AlignCenter)
             self.grid_layout.addWidget(label, 0, j+1, 1, 1)
         spacer = QSpacerItem(70, 40)
-        self.grid_layout.addItem(spacer, self.size+1, 2, 2, self.size) 
-        self.field_widget = QWidget()
-        self.field_widget.setLayout(self.grid_layout)
-        self.scroll_area = QScrollArea()
-        self.scroll_area.setWidgetResizable(True)
-        self.scroll_area.setWidget(self.field_widget)
-        self.setCentralWidget(self.scroll_area)
+        self.grid_layout.addItem(spacer, 10+1, 2, 2, 10) 
+        self.setLayout(self.grid_layout)
+        
         
     def set_ship_size(self, size=2):
         self.ship_size = size
@@ -208,24 +212,17 @@ class MainWindow(QMainWindow):
         if not endpoints:
             return False
         for endpoint in endpoints:
-            if endpoint[0] < 0 or endpoint[0] >= self.size or endpoint[1] < 0 or endpoint[1] >= self.size:
+            if endpoint[0] < 0 or endpoint[0] >= 10 or endpoint[1] < 0 or endpoint[1] >= 10:
                 return False
             if self.button_fields[endpoint[0]][endpoint[1]].property("occupied"):
                 return False
         x_values = [point[0] for point in endpoints]
         y_values = [point[1] for point in endpoints]
-        if min(x_values) < 0 or max(x_values) >= self.size or min(y_values) < 0 or max(y_values) >= self.size:
+        if min(x_values) < 0 or max(x_values) >= 10 or min(y_values) < 0 or max(y_values) >= 10:
             return False
         return True
 
-    def occupy_cells(self, x, y, orientation):
-        endpoints = self.find_possible_endpoints(x, y, orientation)
-        for endpoint in endpoints:
-            button = self.button_fields[endpoint[0]][endpoint[1]]
-            button.setProperty("occupied", True)
-            button.setStyleSheet("background-color: #66b3ff")
-            button.setText(self.selected_ship)
-            button.setEnabled(False)
+    
     
     def place_ship(self, x, y):
         if self.selected_ship is None:
@@ -237,7 +234,7 @@ class MainWindow(QMainWindow):
         if self.vertical_checkbox.isChecked():
             if self.check_valid_placement(x, y, "down"):
                 self.occupy_cells(x, y, "down")
-                ship_num = self.ship_counts[self.selected_ship] + 1
+                self.ship_num = self.ship_counts[self.selected_ship] + 1
                 self.ships.append((self.selected_ship, self.find_possible_endpoints(x, y, "down")))
                 for i, j in self.find_possible_endpoints(x, y, "down"):
                     self.button_fields[i][j].setProperty("occupied", True)
@@ -247,7 +244,7 @@ class MainWindow(QMainWindow):
         else:
             if self.check_valid_placement(x, y, "right"):
                 self.occupy_cells(x, y, "right")
-                ship_num = self.ship_counts[self.selected_ship] + 1
+                self.ship_num = self.ship_counts[self.selected_ship] + 1
                 self.ships.append((self.selected_ship, self.find_possible_endpoints(x, y, "right")))
                 for i, j in self.find_possible_endpoints(x, y, "right"):
                     self.button_fields[i][j].setProperty("occupied", True)
@@ -255,15 +252,28 @@ class MainWindow(QMainWindow):
                 if all(count == 2 for count in self.ship_counts.values()):
                     QMessageBox.information(self, "All ships placed", "All ships have been placed!")
         self.update_confirm_button()
+        self.id = int(str(self.ship_num) + str(self.selected_ship))
+        print(self.id)
+    def occupy_cells(self, x, y, orientation):
+        endpoints = self.find_possible_endpoints(x, y, orientation)
+        for endpoint in endpoints:
+            button = self.button_fields[endpoint[0]][endpoint[1]]
+            button.setProperty("occupied", True)
+            button.setStyleSheet("background-color: #66b3ff; border: 1px solid black")
+            button.setText(str(self.ship_num) + str(self.selected_ship))
+            button.setEnabled(False)
+
+
+
         
     def find_possible_endpoints(self, x, y, orientation):
         endpoints = []
         if orientation == "right":
-            if y + self.ship_size <= self.size:
+            if y + self.ship_size <= 10:
                 for j in range(y, y + self.ship_size):
                     endpoints.append((x, j))
         elif orientation == "down":
-            if x + self.ship_size <= self.size:
+            if x + self.ship_size <= 10:
                 for i in range(x, x + self.ship_size):
                     endpoints.append((i, y))
         return endpoints
@@ -271,11 +281,12 @@ class MainWindow(QMainWindow):
     def reset_game(self):
         self.ship_counts = {"2": 0, "3": 0, "4": 0, "5": 0}
         self.ships = []
-        for i in range(self.size):
-            for j in range(self.size):
+        for i in range(10):
+            for j in range(10):
                 button = self.button_fields[i][j]
                 button.setProperty("occupied", False)
-                button.setStyleSheet("background-color: #e6f3ff")
+                button.setStyleSheet("QPushButton {background-color:#ccf2ff ; color: white; border: 0.5px solid black;}"
+                                   "QPushButton:hover {background-color:#cce6ff  ;}")
                 button.setText("")
                 button.setEnabled(True)
         self.radioButton1.setEnabled(True)
@@ -286,7 +297,10 @@ class MainWindow(QMainWindow):
     def update_confirm_button(self):
         if self.ship_counts["2"] == 2 and self.ship_counts["3"] == 2 \
                 and self.ship_counts["4"] == 2 and self.ship_counts["5"] == 2:
+            self.buttonC.setStyleSheet("QPushButton {background-color: #00cc00; color: black; border: 2px solid black;}"
+                                   "QPushButton:hover {background-color:#009900 ;}")
             self.buttonC.setEnabled(True)
+            
         else:
             self.buttonC.setEnabled(False)
 
@@ -295,10 +309,12 @@ class MainWindow(QMainWindow):
             "Are you ready to play?", QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
         if reply == QMessageBox.StandardButton.Yes:
             self.In_Game()
-            
+
+
     
     def In_Game(self):
-        
+        print(self.button_fields)
+        self.close()
         self.window2=In_Game_UI()
         self.window2.show()
     
@@ -307,3 +323,5 @@ app = QApplication(sys.argv)
 window = MainWindow()
 window.show()
 sys.exit(app.exec())
+
+
